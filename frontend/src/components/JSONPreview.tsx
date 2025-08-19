bf40860c8455504a5e8894fce9bd87bb82294746
Rohit Mahesh <rohit.mahesh@newhaptics.com>
2025-08-19 18:17:57 -0400

    removed legacy component

--- frontend/src/components/JSONPreview.tsx
+++
@@ -1,7 +0,0 @@
-export default function JSONPreview({ data }: { data: unknown }) {
-  return (
-    <pre style={{ whiteSpace: "pre-wrap", background: "#0b0e14", padding: 12, borderRadius: 8 }}>
-      {JSON.stringify(data, null, 2)}
-    </pre>
-  );
-}
